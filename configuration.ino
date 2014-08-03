20afa9cf04b17d78b8f31487c4ad6a756098ebb9
j0nson <s.darlington@gmail.com>
2014-08-03 17:32:46 -0400

    add UNL2003 configurations

--- configuration.ino
+++ configuration.ino
@@ -20,10 +20,10 @@ Comment out the blocks of code you don't need.
 // =================================================================
 // 1. Adafruit motorshield
 
-// Using Adafruit Motorshield V2? Comment out this one line underneath.
-#include <AFMotor.h>
+// Using Adafruit Motorshield V2 or UNL2003? Comment out this one line underneath.
+//#include <AFMotor.h>
 
-// Using Adafruit Motorshield V1? Comment out the three INCLUDE lines below.
+// Using Adafruit Motorshield V1 or UNL2003? Comment out the three INCLUDE lines below.
 //#include <Wire.h>
 //#include <Adafruit_MotorShield.h>
 //#include "utility/Adafruit_PWMServoDriver.h"
@@ -52,6 +52,13 @@ void forwardb() { afMotorB->onestep(FORWARD, stepType); }
 void backwardb() { afMotorB->onestep(BACKWARD, stepType); }
 #endif
 
+#ifdef UNL2003
+//const int stepType = MICROSTEP;
+void forwarda() { motorA.move(FORWARD);motorA.run();
+void backwarda() { motorA.move(BACKWARD);motorA.run();
+void forwardb() { motorB.move(FORWARD);motorB.run();
+void backwardb() { motorB.move(BACKWARD);motorB.run();
+#endif
 
 AccelStepper motorA(forwarda, backwarda);
 AccelStepper motorB(forwardb, backwardb);
